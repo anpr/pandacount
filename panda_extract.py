fda464989b2b0742e75cd64f24f43f4c01f36f41
Andreas Profous <ap@profous.de>
2022-04-23 20:07:30 +0200

    Delete panda_extract

--- panda_extract.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np                        #to cleanse data
-from datetime import datetime             #to manipulate dates
-import plotly.express as px               #to create interactive charts
-import plotly.graph_objects as go         #to create interactive charts
-from jupyter_dash import JupyterDash      #to build Dash apps from Jupyter environments
-from dash import dcc        #to get components for interactive user interfaces
-from dash import html
-
