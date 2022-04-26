b77e7072d96816534daeadafc30be43ce4f1c93d
Andreas Profous <ap@profous.de>
2022-04-26 16:52:32 +0200

    Add some categories, gesa account and various bugfixes

--- panda_ing_import.py
+++ panda_ing_import.py
@@ -27,7 +27,11 @@ def skip_lines_until(file_name: str, predicate: Callable[[str], bool]):
 def get_account(file_name: str) -> str:
     stem = Path(file_name).stem
     _, iban, _ = stem.split("_")
-    iban_account_map = {"DE97500105175409854125": "common", "DE69500105175402313946": "giro"}
+    iban_account_map = {
+        "DE97500105175409854125": "common",
+        "DE69500105175402313946": "giro",
+        "DE27500105175404412327": "gesa",
+    }
 
     return iban_account_map[iban]
 
@@ -73,6 +77,7 @@ def to_raw_df(file_name: str) -> pd.DataFrame:
 
 
 def categorize(df: pd.DataFrame) -> pd.DataFrame:
+    """Sets category column of dataframe."""
     category_attribute_subs_map = {
         "bargeld": {"party": ["bargeldauszahlung"]},
         "einkaufen": {
@@ -96,10 +101,12 @@ def categorize(df: pd.DataFrame) -> pd.DataFrame:
                 "KoRo Drogerie GmbH",
                 "BIO COMPANY GmbH",
                 "gewuerzland",
+                "BIO COMPANY SE",
+                "Ihr Einkauf bei Flink SE",
             ],
         },
-        "einnahmen": {"party": ["andreas edmond profous"]},
         "einnahmen::dividende": {"purpose": ["dividende"]},
+        "einnahmen::gehalt::andreas": {"party": ["andreas edmond profous"]},
         "geschenk": {
             "party": ["VISA SPIELVOGEL"],
             "purpose": ["superiore.de", "geschenk mama", "Marimekko"],
@@ -113,21 +120,42 @@ def categorize(df: pd.DataFrame) -> pd.DataFrame:
             ],
             "purpose": ["Center-Apotheke im Minipreis", "SPEICKSHOP", "SHAVING.IE"],
         },
+        "haftpflichtversicherung": {"party": ["asspario Versicherungsdienst AG"]},
         "kleidung": {"party": ["VISA MAGAZZINO"]},
         "kinder": {
             "party": [
                 "Carolina Sgro",
                 "Musikschule City West",
-                "Erika Tribbioli",
                 "KINDER- UND JUGEND-, REIT- UND FAHRVEREIN ZEHLENDORF E.V.",
             ],
             "purpose": ["Zoologischer Garten Be", "Kinderschwimmen"],
         },
-        "kinder::sparen": {"purpose": ["Sparen Depot Paula"]},
-        "kinder::schule": {"purpose": ["Kassenzeichen: 2134900496613 Paula Profous"]},
+        "kinder::kindergeld": {"party": ["Bundesagentur fur Arbeit - Familienkasse"]},
+        "kinder::sparen": {"purpose": ["Sparen Depot Paula", "Sparplan ISIN LU0360863863"]},
+        "kinder::schulekita": {
+            "party": ["NBH Schoeneberg"],
+            "purpose": [
+                "Kassenzeichen: 2134900496613 Paula Profous",
+                "Beitrag fur die Sprachforderung",
+            ],
+        },
+        "kinder::theater": {"party": ["Erika Tribbioli"]},
+        "justetf": {"party": ["justETF GmbH"]},
         "media": {
-            "party": ["amznprime", "prime video", "abo lage der nation", "aws emea", "thalia.de"],
-            "purpose": ["Spotify AB", "audible.de", "netflix.com"],
+            "party": [
+                "amznprime",
+                "prime video",
+                "abo lage der nation",
+                "aws emea",
+                "thalia.de",
+                "VISA AUDIBLE.IT",
+            ],
+            "purpose": [
+                "Spotify AB",
+                "audible.de",
+                "netflix.com",
+                "PP.2107.PP . SPOTIFY, Ihr Einkauf b ei SPOTIFY",
+            ],
         },
         "mobilitaet::auto": {
             "party": [
@@ -139,10 +167,12 @@ def categorize(df: pd.DataFrame) -> pd.DataFrame:
                 "ARAL AG",
                 "Worldline Sweden AB fuer Shell",
                 "VISA ARAL STATION",
+                "VISA STAR TANKSTELLE",
             ],
             "purpose": ["CosmosDirekt Kfz Beitrag"],
         },
         "mobilitaet::autoleihen": {"party": ["VISA ENTERPRISE RENT A CAR", "VISA RENTALCARS.COM"]},
+        "mobilitaet::db::oebb:": {"purpose": ["OBB-Personenverkehr AG", "OEBB PV AG"]},
         "mobilitaet::fahrrad": {"party": ["bike market city", "FAHRRADLADEN MEHRINGHOF"]},
         "mobilitaet::fliegen": {
             "party": ["RYANAIR"],
@@ -156,7 +186,7 @@ def categorize(df: pd.DataFrame) -> pd.DataFrame:
             "party": ["bvg app", "DB Fernverkehr AG"],
             "purpose": ["DB Vertrieb GmbH"],
         },
-        "intern": {"party": ["andreas profous"]},
+        # "intern": {"party": ["andreas profous"]},
         "intern::rente": {"purpose": ["Wertpapierkauf"], "book_text": ["Wertpapierkauf"]},
         "intern::steuerklasse": {"purpose": ["Ausgleich Steuerklasse"]},
         "restaurant": {
@@ -175,24 +205,69 @@ def categorize(df: pd.DataFrame) -> pd.DataFrame:
                 "VISA SY RESTAURANT",
             ]
         },
+        "rente::gesa": {"party": ["DWS Investment GmbH"]},
         "spenden": {"party": ["Aerzte ohne Grenzen eV"]},
         "sport": {"party": ["Katherine Finger"]},
         "urlaub": {"purpose": ["Airbnb Payments", "airbnb"]},
         "wohnen": {"purpose": ["Rate, Putzen, Naturstrom", "Ausgleich WEG"]},
+        "wohnen::hausratversichterung": {"party": ["COYA Hausrat"]},
+        "wohnen::grundsteuer": {"purpose": ["STEUERNR 024/749/07849 GRUNDST"]},
+        "wohnen::GEZ": {"party": ["Rundfunk ARD, ZDF, DRadio"]},
+        "wohnen::strom": {"party": ["NaturStromHandel GmbH"]},
+        "wohnen::putzen": {"party": ["INES BORNEMANN"]},
+        "wohnen::rate": {"purpose": ["Rechnung Darl.-Leistung 6070166475"]},
+        "wohnen::wohngeld": {"party": ["WEG Holsteinische Strase 43 in 10717 Berlin"]},
     }
 
     for category, subs_map in category_attribute_subs_map.items():
         for attribute, subs in subs_map.items():
             for sub in subs:
                 df.loc[
-                    df[attribute].fillna("").str.lower().str.contains(sub.lower()), "category"
+                    df[attribute].fillna("").str.lower().str.contains(sub.lower(), regex=False), "category"
                 ] = category
 
     df.loc[
-        df.party.fillna("").str.lower().str.contains("VISA APPLE.COM/BILL".lower())
+        (df.party.fillna("").str.lower().str.contains("VISA APPLE.COM/BILL".lower(), regex=False))
         & (df.amount > -50),
         "category",
     ] = "media"
+
+    df.loc[
+        (df.account == "gesa") & (df.book_text == "Gehalt/Rente"),
+        "category",
+    ] = "einnahmen::gehalt::gesa"
+
+    df.loc[
+        (df.account == "giro") & (df.party == "Kreuzwerker"),
+        "category",
+    ] = "einnahmen::gehalt::andreas"
+
+    df.loc[
+        (df.party.fillna("").str.lower().str.contains("Finanzamt Charlottenburg".lower(), regex=False))
+        & (df.book_text == "Gutschrift"),
+        "category",
+    ] = "einnahmen::steuererstattung"
+
+    return df
+
+
+def transfer_categorize(df: pd.DataFrame) -> pd.DataFrame:
+    """Adds transfer_category column to df."""
+
+    transfer_category_attribute_subs_map = {
+        "giro::gesa": {"purpose": ["Ausgleich Steuerklasse"]},
+        "giro::common": {
+            "purpose": ["Rate, Putzen, Naturstrom", "Ausgleich WEG", "Sparen Depot Paula"]
+        },
+    }
+
+    for transfer_category, subs_map in transfer_category_attribute_subs_map.items():
+        for attribute, subs in subs_map.items():
+            for sub in subs:
+                df.loc[
+                    df[attribute].fillna("").str.lower().str.contains(sub.lower(), regex=False),
+                    "transfer_category",
+                ] = transfer_category
 
     return df
 
@@ -207,10 +282,13 @@ def to_yaml(df: pd.DataFrame) -> str:
     Returns:
         The dataframe as a yaml file.
     """
-    df["book_date"] = df.book_date.dt.strftime("%Y-%m-%d")
-    df["valuta_date"] = df.valuta_date.dt.strftime("%Y-%m-%d")
+    yaml_df = df.copy()
+    yaml_df["book_date"] = df.book_date.dt.strftime("%Y-%m-%d")
+    yaml_df["valuta_date"] = df.valuta_date.dt.strftime("%Y-%m-%d")
+    if "category_manual" not in yaml_df.columns:
+        yaml_df["category_manual"] = ""
     yml = yaml.dump(
-        df.reset_index().to_dict(orient="records"),
+        yaml_df.reset_index().to_dict(orient="records"),
         sort_keys=False,
         width=120,
         indent=2,
@@ -258,7 +336,9 @@ def import_to_pandacount(pc: pd.DataFrame, df: pd.DataFrame) -> pd.DataFrame:
         subset=["account", "book_date", "valuta_date", "party", "book_text", "purpose", "amount"],
         inplace=True,
     )
-    pc.sort_values(axis=0, by=["book_date", "account", "valuta_date", "party", "purpose"], inplace=True)
+    pc.sort_values(
+        axis=0, by=["book_date", "account", "valuta_date", "party", "purpose"], inplace=True
+    )
     return pc
 
 
@@ -266,11 +346,14 @@ def main(file_list: List[str]):
     pc = load_pc()
     for file_name in file_list:
         typer.echo(f"Processing {file_name}")
-        df = pipe(file_name, to_raw_df, categorize)
+        df = pipe(file_name, to_raw_df)
+        print(f"  Importing dataframe with {df.shape[0]} rows...")
         pc = import_to_pandacount(pc, df)
 
+    print(f"Categorizing {pc.shape[0]} entries...")
+    pc = pipe(pc, transfer_categorize, categorize)
     save_pc(pc)
-    print(pc.head())
+    print(f"\nStored pandacount.yml with {pc.shape[0]} rows in total")
 
 
 if __name__ == "__main__":
